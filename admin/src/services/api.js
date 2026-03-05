f1ff89159d7efe06f24fa73536193ccd1c27722b
anshu-2108 <anshu.bgn@gmail.com>
2026-03-05 19:20:54 +0530

    adm

--- admin/src/services/api.js
+++ admin/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = 'http://localhost:5000/api' // Your main backend URL
+const API_BASE_URL = 'https://portfolio-website-tubm.vercel.app/api' // Your main backend URL
 
 const api = axios.create({
   baseURL: API_BASE_URL,
